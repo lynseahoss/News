653af9603ed5c7ee17afd0a999e425303695b4c2
Lindsey Lawson <lynseahoss@gmail.com>
2020-08-11 14:22:18 -0400

    added basic tab layout from nativebase.io

---
+++ src/screens/Tabs.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Container, Header, Content, Tab, Tabs} from 'native-base';
+import Tab1 from './Tabs/Tab1';
+import Tab2 from './Tabs/Tab2';
+import Tab3 from './Tabs/Tab3';
+export default class TabsExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header hasTabs />
+        <Tabs>
+          <Tab heading="Tab1">
+            <Tab1 />
+          </Tab>
+          <Tab heading="Tab2">
+            <Tab2 />
+          </Tab>
+          <Tab heading="Tab3">
+            <Tab3 />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
