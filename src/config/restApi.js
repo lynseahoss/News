1b7b64e01ad5f648e5f3bc60e4e2263c742ee2d8
Lindsey Lawson <lynseahoss@gmail.com>
2020-08-11 22:36:20 -0400

    created consts for newsapi.org

---
+++ src/config/restApi.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'us';
+export const category = 'general';
+export const _api_key = '6e82ba6680ea4ce8be14980d785524ae';
