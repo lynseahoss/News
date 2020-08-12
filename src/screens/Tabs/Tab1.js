97a35f540900b81c65e190869b810ea8f7971351
Lindsey Lawson <lynseahoss@gmail.com>
2020-08-11 22:56:23 -0400

    added lifecycle hook & imported getArticles

--- src/screens/Tabs/Tab1.js
+++ src/screens/Tabs/Tab1.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Alert, View, ActivityIndicator} from 'react-native';
 import {Container, Content, List, Text} from 'native-base';
 
+import {getArticles} from '../../service/news';
+
 export default class Tab1 extends Component {
   constructor(props) {
     super(props);
@@ -10,6 +12,16 @@ export default class Tab1 extends Component {
       isLoading: true,
       data: null,
     };
+  }
+
+  //lifecycle hook
+  componentDidMount() {
+    getArticles().then(data => {
+      this.setState({
+        isLoading: false,
+        data: data,
+      });
+    });
   }
   render() {
     return (
