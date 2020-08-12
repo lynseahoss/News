bac63831278cd399e513c1252a3fccf613779aa3
Lindsey Lawson <lynseahoss@gmail.com>
2020-08-11 23:41:16 -0400

    imported components from react native/ native base

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,16 @@
 import React, {Component} from 'react';
+import {Dimensions, WebView, Modal, Share} from 'react-native';
+import {
+    Container,
+    Header,
+    Content,
+    Body,
+    Left,
+    Icon,
+    Right,
+    Title,
+    Button,
+  } from 'native-base';
 
 export default class ModalComponent extends Component {
     render(){
