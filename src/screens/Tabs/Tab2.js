68d4f96aae045affc5e859a0b2faaeddef8ee1bd
Lindsey Lawson <lynseahoss@gmail.com>
2020-08-11 22:44:49 -0400

    added constructor with super props

--- src/screens/Tabs/Tab2.js
+++ src/screens/Tabs/Tab2.js
@@ -3,6 +3,14 @@ import {Alert, View, ActivityIndicator} from 'react-native';
 import {Container, Content, List, Text} from 'native-base';
 
 export default class Tab1 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      data: null,
+    };
+  }
   render() {
     return (
       <Container>
