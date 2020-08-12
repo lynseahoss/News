303fd90ee0340882552311ed2bc375dc586a649e
Lindsey Lawson <lynseahoss@gmail.com>
2020-08-11 23:07:13 -0400

    added components to import from native base

--- src/components/DataItem.js
+++ src/components/DataItem.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { ListItem, Left, Right, Thumbnail, Body, View, Text, Button } from 'native-base'
 
 class DataItem extends Component {
     render(){
