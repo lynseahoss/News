c75d9cee32b0e2e885fd2aeef101c9bf982802cd
Lindsey Lawson <lynseahoss@gmail.com>
2020-08-12 22:10:39 -0400

    added functions to ModalComponent

--- src/screens/Tabs/Tab3.js
+++ src/screens/Tabs/Tab3.js
@@ -43,7 +43,11 @@ export default class Tab1 extends Component {
     return (
       <Container>
         <Content>{view}</Content>
-        <ModalComponent />
+        <ModalComponent
+          showModal={this.state.setModalVisible}
+          articleData={this.state.modalArticleData}
+          onClose={this.handleModalClose}
+        />
       </Container>
     );
   }
